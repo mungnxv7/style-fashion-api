53a45e138496908f17e399ca02f12ec2cd546517
longdv ph29867 <longdvph29867@gmail.com>
2024-06-24 21:56:13 +0700

    update api get cart

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -6,7 +6,17 @@ import Attributes from "../models/attribute.model.js";
 import mongoose from "mongoose";
 
 const getCartsByIdUser = async (user_id) => {
-  return await Carts.findOne({ user: user_id });
+  return await Carts.findOne({ user: user_id }).populate([
+    {
+      path: "products_cart.product",
+      model: "Products",
+      select: "name slug thumbnail",
+    },
+    {
+      path: "products_cart.attribute",
+      model: "Attribute",
+    },
+  ]);
 };
 
 const addToCartByIdUser = async (user_ID, cartBody) => {
