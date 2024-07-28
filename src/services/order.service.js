62170d80ec41923f3cec808b443a608b0421e5f1
mungnxv7 <hin10112001@gmail.com>
2024-07-28 11:24:47 +0700

    update logic order

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,26 +1,33 @@
 import Order from "../models/Orders.model.js";
+import { generateCode } from "../utils/generateRandomCode.js";
 
 const createOrder = (bodyOrder) => {
-    return Order.create(bodyOrder);
+  const orderCode = generateCode(9);
+  const orderData = {
+    ...bodyOrder,
+    orderCode: "#" + orderCode,
+    voucher: bodyOrder.voucher === "" ? null : bodyOrder.voucher,
+  };
+  return Order.create(orderData);
 };
 
 const getOrders = (filter, options) => {
-    return Order.paginate(filter, options);
+  return Order.paginate(filter, options);
 };
 
 const getOrderByID = (orderID) => {
-    return Order.findById(orderID);
+  return Order.findById(orderID);
 };
 
 const updateOrder = (orderID, bodyOrder) => {
-    return Order.findByIdAndUpdate(orderID, bodyOrder, { new: true });
+  return Order.findByIdAndUpdate(orderID, bodyOrder, { new: true });
 };
 
 const orderService = {
-    createOrder,
-    getOrders,
-    getOrderByID,
-    updateOrder,
+  createOrder,
+  getOrders,
+  getOrderByID,
+  updateOrder,
 };
 
 export default orderService;
--- src/utils/generateRandomCode.js
+++ src/utils/generateRandomCode.js
@@ -9,3 +9,75 @@ export function generateCode(length) {
   }
   return result;
 }
+
+const create = async (req, res) => {
+  try {
+    const { user, productsOrder, voucher } = req.body;
+    const userData = await userService.getUserById(user);
+    if (!userData) {
+      throw new ApiError(httpStatus.NOT_FOUND, "User not found");
+    }
+    const carts = await cartService.getCartsByIdUser(user);
+    if (!carts || carts.products_cart.length === 0) {
+      throw new ApiError(
+        httpStatus.NOT_FOUND,
+        "There are no products in your cart"
+      );
+    }
+    const cartMap = new Map();
+    carts.products_cart.forEach((itemCart) => {
+      if (!itemCart.attribute) {
+        throw new ApiError(
+          httpStatus.NOT_FOUND,
+          "There an atrribute that are not in product"
+        );
+      }
+      const key = `${itemCart.product._id.toString()}_${itemCart.attribute._id.toString()}`;
+      cartMap.set(key, itemCart._id);
+    });
+    const productCartIds = productsOrder.map((itemOrder) => {
+      const key = `${itemOrder.productId}_${itemOrder.attributeId}`;
+      const getProductCartId = cartMap.get(key);
+      if (!getProductCartId) {
+        throw new ApiError(
+          httpStatus.NOT_FOUND,
+          "There are products that are not in your cart."
+        );
+      }
+      return getProductCartId;
+    });
+    const isProductOrderInProdcutsColection = productsOrder.map(
+      async (itemOrder) => {
+        const product = await productService.getProductByID(
+          itemOrder.productId
+        );
+        if (!product) {
+          throw new ApiError(
+            httpStatus.NOT_FOUND,
+            "There an atrribute that are not in product"
+          );
+        }
+      }
+    );
+    await Promise.all(isProductOrderInProdcutsColection);
+    const order = await orderService.createOrder(req.body);
+    const documentAttribute = await Promise.all(
+      order.productsOrder.map(async (orderItem) => {
+        const attribute = await attributeService.getAttributeByID(
+          orderItem.attributeId
+        ); // Tìm kiếm attribute
+        return {
+          updateOne: {
+            filter: { _id: attribute._id }, // Điều kiện lọc dựa trên _id
+            update: { $set: { stock: attribute.stock - orderItem.quantity } }, // Cập nhật quantity
+          },
+        };
+      })
+    );
+    await cartService.removeCartItemsByIds(user, productCartIds);
+    await attributeService.updateAttributeMany(documentAttribute);
+    res.status(httpStatus.CREATED).json(order);
+  } catch (error) {
+    errorMessage(res, error);
+  }
+};
